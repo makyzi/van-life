dd566c44882c942e5276cb2dcdf2340da66a235d
Martin <sedlacek.martin9@gmail.com>
2023-05-24 06:12:10 +0200

    Update Home.jsx

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Home = () => {
 	return (
 		<div className="home-container">
-			<h1>You got the travel plans, we got the travel vans.</h1>
+			<h1>You got the travel plans, we got the travel vanS.</h1>
 			<p>
 				Add adventure to your life by joining the #vanlife movement and
 				make your perfect road trip.
